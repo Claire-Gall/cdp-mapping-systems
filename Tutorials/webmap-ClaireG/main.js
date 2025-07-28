89f79ff6c2d0ba0b385d45b584b7e91f216f3a9d
Claire.Gall <clairegalla1878@gmail.com>
2025-07-28 18:06:51 -0400

    up

--- Tutorials/webmap-ClaireG/main.js
+++ Tutorials/webmap-ClaireG/main.js
@@ -45,7 +45,66 @@ const jsonFeatures = fetch(
         .addTo(map);
     });
     });
-
-})
-    })
+})}
 .catch((error) => console.error("Error fetching data:", error));
+
+const { createClient } = window.supabase;
+const supabaseUrl = 'https://gaakyqheoetpsezponfo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhYWt5cWhlb2V0cHNlenBvbmZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzOTM2NjMsImV4cCI6MjA2ODk2OTY2M30.Klpf-yFHgP5uR8nR3LqwIWA_KbDMMhJQxU6kw2s7tMA';
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+async function querySupabase() {
+    const { data, error } = await supabaseClient
+        .from("pizza res inspection1")
+        .select("*")
+        .limit(100);
+
+    if (error) {
+        console.error("Error fetching data:", error);
+    } else {
+        console.log("Data fetched successfully:", data);
+    }
+}
+document.addEventListener("DOMContentLoaded", () => {
+    querySupabase();
+});
+
+async function queryWithinDistance(point, n = 1000) {
+        const { data, error } = await supabaseClient.rpc(
+        "find_nearest_n_restaurants",
+        {
+            lat: point[1],
+            lon: point[0],
+            n: n,
+        }
+        );
+
+        if (error) {
+        console.error("Error fetching nearest points:", error);
+        } else {
+        console.log("Nearest points fetched successfully:", data);
+        map.addLayer({
+            'id': 'proximity-layer',
+            'type': 'square',
+            'source': 'pizza-place',
+            paint: {
+                "square-radius": 4,
+                "square-stroke-width": 2,
+                "square-color": "#343BA0",
+                "square-stroke-color": "midnight blue",
+            }
+        });
+        map.on("click", "proximity-layer", (e) => {
+            const coordinates = e.features [0].geometry.coordinates.slice();
+            const description = e.features [0].properties.dba
+            new maplibregl.Popup ()
+            .setLngLat (coordinates)
+            .setHTML (description)
+            .addTo(map);
+        })
+    };
+        map.on("click", (e) => {
+            const point = [e.lngLat.lng, e.lngLat.lat];
+            queryWithinDistance(point, 1000);
+});
+}
