4174b455b1eb79d8af25ae4abf8aa76dcc655cea
Claire.Gall <clairegalla1878@gmail.com>
2025-08-07 09:07:32 -0400

    upup

--- elevationmap/slope.js
+++ elevationmap/slope.js
@@ -1,6 +1,7 @@
   var map = new maplibregl.Map({
         container: 'map', // container id
-        style: '', // style URL for base elevation map
+        style: 'https://demotiles.maplibre.org/style.json', // style URL for base elevation map
         center: [-73.97144, 40.70491], // starting position [lng, lat]
         zoom: 6 // starting zoom
+    map.addControl(new maplibregl.NavigationControl());
     });
