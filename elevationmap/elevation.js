3d72a92eb75dc46c280350948e6f1a5edcf46116
Claire.Gall <clairegalla1878@gmail.com>
2025-08-08 10:42:35 -0400

    upup

--- elevationmap/elevation.js
+++ elevationmap/elevation.js
@@ -1,19 +1,29 @@
-const map = new maplibregl.Map({
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2xhaXJlLWdhbGwiLCJhIjoiY21kNmw1M2JtMDlqczJxcHR1NjMxZjA2NSJ9.4RsfacX1wV7fzgRQtJ2_lQ';
+
+const map = new mapboxgl.Map({
     container: 'map',
-    style: 'style.json', // Or a valid URL to a working style
-    zoom: 5,
+    style: 'mapbox://styles/claire-gall/cmdck3krn00ti01sa44zj056i', // cleaner style URL
+    zoom: 2,
     center: [108, 4]
 });
-
-map.addControl(new maplibregl.NavigationControl());
 
 map.on('style.load', () => {
     map.setFog({});
 });
 
 map.on('load', () => {
-    map.addSource('output_geojson_path', {
+    map.addSource('elevation-source', {
         type: 'geojson',
-        data: 'output_geojson_path.geojson'
+        data: 'output_path.geojson' // make sure this file exists and is valid
+    });
+
+    map.addLayer({
+        id: 'elevation-layer',
+        type: 'fill',
+        source: 'elevation-source',
+        paint: {
+            'fill-color': 'yellow',
+            'fill-opacity': 0.5
+        }
     });
 });
